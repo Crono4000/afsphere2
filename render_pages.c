de1f1751ef9043ab0906c3953fd9c8af8db7a061
Crono4000 <antoniofrancosanto@gmail.com>
2026-09-23 13:02:54 +0100

    started programming write_page_with_result function

--- render_pages.c
+++ render_pages.c
@@ -50,6 +50,35 @@ void	write_pgresult(PGresult *result, char *template, int fd)
     close(fd);
 }
 
-//void    write_info_table_page
+int	write_page_with_result(PGconn *conn, int fd, char *file, PGresult *result, char *template)
+{
+	char	read_buffer[CPY_BUFFER + 1];
+	int		read_fd;
+	int		read_bytes;
+	int		read_index;
+
+	read_fd = open(file, O_RDONLY);
+	if (read_fd == -1)
+		return (3);
+	read_bytes = read(read_fd, read_buffer, CPY_BUFFER);
+	read_buffer[read_bytes] = '\0';
+	while (read_bytes)
+	{
+		read_index = 0;
+		while (read_index < read_bytes)
+		{
+			if (read_buffer[read_index] == '$' && read_buffer[read_index + 1] == '$')
+			{
+				write_pgresult(result, template, );
+				read_index++;
+			}
+			else
+				write(fd, read_buffer + read_index, 1);
+			read_index++;
+		}
+		read_bytes = read(read_fd, read_buffer, CPY_BUFFER);
+		read_buffer[read_bytes] = '\0';
+	}
+}
 
 
